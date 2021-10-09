a56ff0e6e1cfc68611965200ca05ff4ba308eae8
ngandinh1610js <ngandinh1610.js@gmail.com>
2021-10-09 13:17:21 +0700

    Test

---
+++ Lab04/DriverDistance.php
@@ -0,0 +1,25 @@
+<html>
+    <head><title>Distance from Chicago</title></head>
+    <body>
+        <span>The page that calculates the distance from Chicago</span>
+        <br><br>
+        <span>Select a destination :</span>
+        <br><br>
+        <form action=" " method="GET">
+            <section name="destination[]" mulitple>
+                <option>Boston</option>
+                <option>Dallas</option>
+                <option>Miami</option>
+                <option>Nashville</option>
+                <option>Las Vegas</option>
+                <option>Pittsburgh</option>
+                <option>San Francisco</option>
+                <option>Toronto</option>
+                <option>Washington, DC</option>
+            </section>
+            <br><br>
+            <input type="submit" value="Submit"> 
+            <input type="reset" value="Reset">
+        </form>
+    </body>
+</html>
