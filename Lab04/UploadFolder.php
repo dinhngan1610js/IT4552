197c70adb44e0317a30b62b929ea72f28e6c282c
ngandinh1610js <ngandinh1610.js@gmail.com>
2021-10-12 01:59:28 +0700

    Create Lab04-2

---
+++ Lab04/UploadFolder.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Upload Folder</title>
+    </head>
+    <body>
+
+        <?php
+            
+        ?>
+        
+        <h3>Upload Folder & View detail</h3>
+        <form action="#" method="post" enctype="multipart/form-data"> 
+            Select Folder to Upload: 
+            <input type="file" name="files[]" id="files" multiple directory="" webkitdirectory="" moxdirectory="" /><br/><br/> 
+            <input type="Submit" value="Upload" name="upload" />
+        </form>
+  </body>
+</html>
