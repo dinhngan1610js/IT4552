764be0ca3c3ba77ae6d51a84d9c990b4ddc903e8
ngandinh1610js <ngandinh1610.js@gmail.com>
2021-10-15 02:00:20 +0700

    size & date not completed

--- Lab04/UploadFolder.php
+++ Lab04/UploadFolder.php
@@ -3,15 +3,20 @@
         <title>Upload Folder using PHP </title>
     </head>
     <body>
-
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data"> 
-            
-            Select Folder to Upload: <input type="file" name="dir_uppload[]" multiple directory="" webkitdirectory="" moxdirectory="" /><br/><br/> 
-            <input type="Submit" value="Upload" name="submit" >
-        </form>
-
         <?php
-            var_dump($_FILES);
+        $path  = 'upload';
+        $files = scandir($path);
+        $files = array_diff(scandir($path), array('.', '..'));
+        
+        echo "List of folder $path";
+        foreach($files as $file){
+            $ext = pathinfo($file);
+            $tail = $ext['extension'];
+            echo "<br>Name : $file <br>";
+            echo "Type : $tail <br>";
+            echo "Size : " .filesize($file) ."<br>";
+            echo "Date : " .filemtime($file) ."<br>";
+        }
         ?>
     </body>
   </html>
