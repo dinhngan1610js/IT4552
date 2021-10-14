324afff254be3cd5ca64fc7d1689364edd55cc0b
ngandinh1610js <ngandinh1610.js@gmail.com>
2021-10-15 02:49:24 +0700

    Lab04 completed

--- Lab04/UploadFolder.php
+++ Lab04/UploadFolder.php
@@ -4,18 +4,27 @@
     </head>
     <body>
         <?php
-        $path  = 'upload';
-        $files = scandir($path);
-        $files = array_diff(scandir($path), array('.', '..'));
+        $dir  = "./upload";
+        // $files = scandir($path);
+        // $files = array_diff(scandir($path), array('.', '..'));
         
-        echo "List of folder $path";
-        foreach($files as $file){
-            $ext = pathinfo($file);
-            $tail = $ext['extension'];
-            echo "<br>Name : $file <br>";
-            echo "Type : $tail <br>";
-            echo "Size : " .filesize($file) ."<br>";
-            echo "Date : " .filemtime($file) ."<br>";
+        // echo "List of folder $path";
+        // foreach($files as $file){
+        //     $ext = pathinfo($file);
+        //     $tail = $ext['extension'];
+        //     echo "<br>Name : $file <br>";
+        //     echo "Type : $tail <br>";
+        //     echo "Size : " .filesize($file) ."<br>";
+        //     echo "Date : " .filemtime($file) ."<br>";
+        // }
+
+        if (is_dir($dir)) {
+            if ($dh = opendir($dir)) {
+                while (($file = readdir($dh)) !== false) {
+                    echo "Filename : " .$file . "<br>";
+                }
+                closedir($dh);
+            }
         }
         ?>
     </body>
